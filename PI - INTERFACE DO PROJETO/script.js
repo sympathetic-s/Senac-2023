5327798f8baac0baf609fc4c9e4b63f02cb54106
WELLINGTON BRITO <86670829+Wellington472@users.noreply.github.com>
2023-11-20 16:37:16 -0300

    Add files via upload
    Nessa pasta contem os arquivos html e css, referente a interface do projeto.

---
+++ PI - INTERFACE DO PROJETO/script.js
@@ -0,0 +1,4 @@
+
+var nomes = ("Projeto Integrador - SENAC. Juliane Mascarenhas Machado/ Marcelo Oliveira De Melo/ Raul Bernardo Torta Monteiro/ Rogerio Caldeira da Silva/ Samuel Cardoso Ferreira/ Wellington Brito dos Santos");
+
+window.alert (nomes);
